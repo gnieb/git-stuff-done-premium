965cf85b9c3c800e04de1ef8eb0393f9969ecbe0
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-11 12:28:27 -0500

    added form fields

--- src/components/AddNewTask.js
+++ src/components/AddNewTask.js
@@ -1,11 +1,40 @@
+import {useState} from "react";
+
+
 function AddNewTask () {
+const [isPrioChecked, setIsPrioChecked] = useState(false)
+
+function handleCheck(e) {
+    setIsPrioChecked(e.target.checked)
+}
+
     return (
         <form>
+            <label>Let's git this done</label>
             <input 
             type="text"
-            placeholder="What do you want to get done...?">
-            </input>
-            <input type="submit" />
+            placeholder="New Task..."
+            />
+            <select>
+                <option>Select Category</option>
+                <option>Web Dev</option>
+                <option>Health</option>
+                <option>Around the House</option>
+            </select>
+            <label>Notes to Self</label>
+            <textarea
+            placeholder="Add notes..."
+            />
+            <label>Due Date</label>
+            <input type="date" />
+            <label htmlFor="priority">Priority Task?</label>
+            <input
+            id="priority"
+            type="checkbox"
+            checked={isPrioChecked}
+            onChange={handleCheck}
+            />
+            <button type="submit">Add to List!</button>
         </form>
     )
 }
