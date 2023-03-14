1819a406582dc33f0a88be4e11597b05bcbb3a4c
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-13 23:17:01 -0400

    calendar formatting

--- src/components/CalendarDisplay.js
+++ src/components/CalendarDisplay.js
@@ -1,8 +1,34 @@
 import Calendar from "react-calendar";
+import {useState} from "react";
+import 'react-calendar/dist/Calendar.css'; 
 
 function CalendarDisplay () {
+const [value, setValue] = useState(new Date());
+const [isClicked, setIsClicked] = useState(false)
+
+function onChange(nextValue) {
+    setValue(nextValue);
+  }
+
+function onClickDay () {
+    setIsClicked(!isClicked)
+    console.log(value)
+}
+
+
     return (
-        <Calendar />
+        <div>
+        <Calendar 
+        onChange={onChange}
+        value={value}
+        onClickDay={onClickDay}
+        />
+        <div>
+            {isClicked ?
+            <h3>{value} details</h3>:
+            <div></div> }
+        </div>
+        </div>
     )
 }
 
